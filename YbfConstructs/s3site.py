3f314bb4176ca25c22fb37f54fcad834d7fcf1db
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2023-12-04 16:32:08 -0500

    feat: add cache control and new construct

--- YbfConstructs/s3site.py
+++ YbfConstructs/s3site.py
@@ -1,12 +1,15 @@
-from aws_cdk import aws_s3_deployment as s3deploy
-from aws_cdk import aws_s3 as s3
+from typing import Optional
+from typing import Sequence
+
+from aws_cdk import Duration
+from aws_cdk import aws_certificatemanager as acm
 from aws_cdk import aws_cloudfront as cloudfront
 from aws_cdk import aws_cloudfront_origins as origins
-from aws_cdk import aws_certificatemanager as acm
 from aws_cdk import aws_route53 as rt53
 from aws_cdk import aws_route53_targets as rt53_targets
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_deployment as s3deploy
 from constructs import Construct
-import typing
 
 
 class SiteDeploy(Construct):
@@ -18,19 +21,19 @@ class SiteDeploy(Construct):
 
     def __init__(
             self, scope: Construct, id: str, *,
-            sources: typing.Sequence[s3deploy.ISource],
+            sources: Sequence[s3deploy.ISource],
             certificate: acm.ICertificate,
             domain_name: str,
             route53_zone: rt53.IHostedZone,
-            bucket_name: typing.Optional[str] = None,
-            prune_bucket: typing.Optional[bool] = False,
-            root_object: typing.Optional[str] = 'index.html',
-            website_error_document: typing.Optional[str] = '404.html'
+            bucket_name: Optional[str] = None,
+            prune_bucket: Optional[bool] = False,
+            root_object: Optional[str] = 'index.html',
+            website_error_document: Optional[str] = '404.html',
             ) -> None:
         super().__init__(scope, id)
 
         if bucket_name is None:
-            pkgBucket = s3.Bucket(
+            self.pkgBucket = s3.Bucket(
                 self, "SiteBucket",
                 public_read_access=True,
                 block_public_access = s3.BlockPublicAccess(
@@ -39,16 +42,16 @@ class SiteDeploy(Construct):
                     block_public_policy=False,
                     restrict_public_buckets=False,
                 ),
-                website_error_document="404.html",
+                website_error_document=website_error_document,
                 website_index_document=root_object)
         else:
-            pkgBucket = s3.Bucket.from_bucket_name(
+            self.pkgBucket = s3.Bucket.from_bucket_name(
                 self, "SiteBucket", bucket_name)
 
-        distribution = cloudfront.Distribution(
+        self.distribution = cloudfront.Distribution(
             self, "Distribution",
             default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(pkgBucket),
+                origin=origins.S3Origin(self.pkgBucket),
                 allowed_methods=cloudfront.AllowedMethods.ALLOW_ALL,
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS  # noqa: E501
             ),
@@ -62,13 +65,35 @@ class SiteDeploy(Construct):
             record_name=domain_name,
             zone=route53_zone,
             target=rt53.RecordTarget.from_alias(
-                rt53_targets.CloudFrontTarget(distribution))
+                rt53_targets.CloudFrontTarget(self.distribution))
         )
 
         s3deploy.BucketDeployment(
             self, "WebsiteFiles",
             sources=sources,
-            destination_bucket=pkgBucket,
-            distribution=distribution,
-            prune=prune_bucket
+            destination_bucket=self.pkgBucket,
+            distribution=self.distribution,
+            prune=prune_bucket,
+            cache_control=[
+                s3deploy.CacheControl.max_age(Duration.days(365)),
+                s3deploy.CacheControl.immutable()
+            ]
+        )
+
+class StaticHTMLSiteDeploy(SiteDeploy):
+
+    def __init__(self, scope: Construct, id: str, local_dir: str, **kwargs) -> None:
+        sources = [s3deploy.Source.asset(local_dir)]
+        super().__init__(scope, id, sources=sources, **kwargs)
+
+        html_assets = [s3deploy.Source.asset(local_dir, exclude=['*', '!*.html', '!**/*.html'])]
+        s3deploy.BucketDeployment(
+            self, "HTMLFiles",
+            sources=html_assets,
+            destination_bucket=self.pkgBucket,
+            distribution=self.distribution,
+            prune=False,
+            cache_control=[
+                s3deploy.CacheControl.max_age(Duration.days(0)),
+            ]
         )
